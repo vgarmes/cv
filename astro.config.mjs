b50f0ccf41445e7727e095e8c65490132b42534c
Victor Garcia Mestre <vgmestre@gmail.com>
2025-09-17 22:39:05 +0200

    upgrade to astro 5 and tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  // ...
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
