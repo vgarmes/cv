190ed6efbb69acce8ba0a3b5dc19157f8fc8e2f0
Victor Garcia Mestre <vgmestre@gmail.com>
2025-09-18 08:08:48 +0200

    chore: update cv, install prettier tw plugin

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,6 @@
-/** @type {import("prettier").Config} */
+/** @type {import("prettier").Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
-  plugins: ["prettier-plugin-astro"],
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
   overrides: [
     {
       files: "*.astro",
