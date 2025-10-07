80c9c50a771a6aa88f5c2f3c84a82c8c995b89ed
Victor Garcia Mestre <vgmestre@gmail.com>
2025-10-07 13:15:58 +0200

    feat: redesigned styles

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,17 +1,15 @@
-function abbreviateMonth(date: string) {
-  const [month, year] = date.split(" ");
-  const abbreviatedMonth = month.slice(0, 3);
-  return `${abbreviatedMonth} ${year}`;
-}
-
 export function formatDates(startDate: string, endDate: string | null) {
-  const formatter = new Intl.DateTimeFormat("en-GB", {
-    month: "long",
+  const formatter = new Intl.DateTimeFormat("en-US", {
+    month: "short",
     year: "numeric",
   });
-  const start = abbreviateMonth(formatter.format(new Date(startDate)));
-  const end = endDate
-    ? abbreviateMonth(formatter.format(new Date(endDate)))
-    : "Present";
+  const start = formatter.format(new Date(startDate));
+  const end = endDate ? formatter.format(new Date(endDate)) : "Present";
   return `${start} - ${end}`;
+}
+
+export function formatDatesOnlyYear(startDate: string, endDate: string | null) {
+  const startYear = new Date(startDate).getFullYear();
+  const endYear = endDate ? new Date(endDate).getFullYear() : "Present";
+  return `${startYear} - ${endYear}`;
 }
